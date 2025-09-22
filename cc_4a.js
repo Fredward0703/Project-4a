6b9692cd15b4398bd1c62b11c771912b7627e726
Federico Montejo <fmontejo@usf.edu>
2025-09-21 22:39:39 -0400

    Think i finished step 3, will say looked up "math.round" but could not for the life of me get it to round to 2 decimal places. Tried "/100" as told but divided the whole thing instead :(

--- cc_4a.js
+++ cc_4a.js
@@ -2,7 +2,36 @@
 const products = [
     {name:"Soccer Ball", Category:"Sports",price:19.99,inventory:20},
     {name:"Athletic Shorts", Category:"Clothing",price:9.99,inventory:35},
-    {name:"Laptops", Category:"Technology",price:499.99,inventory:8},
-    {name:"Shampoo", Category:"Essentials",price:4.99,inventory:42},
+    {name:"Laptops", Category:"Electronics",price:499.99,inventory:8},
+    {name:"Shampoo", Category:"Household",price:4.99,inventory:42},
     {name:"Chair", Category:"Household",price:49.99,inventory:12},
 ];
+console.log("===== Products I am selling =====")
+console.log(products)
+
+//attempting step 3, wish me luck
+for (let product of products) {
+    let discount;
+
+    switch (product.Category) {
+        case "Electronics":
+            discount = .2
+            console.log(`Laptops are now on sale for $${Math.round(product.price * (1-discount))}!`)
+            break;
+        case "Sports":
+        case "Clothing":
+            discount = .15
+            console.log(`Sports and Clothing are now $${Math.round(product.price * (1-discount))}!`)
+            break;
+        case "Household":
+            discount = .1
+            console.log(`Household products are now $${Math.round(product.price * (1-discount))}!`)
+            break;
+        
+    
+        default:
+            discount = 0;
+            break;
+    }
+    
+}

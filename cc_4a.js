afa933221b126fad9a19a146c4580733d3357e68
Federico Montejo <fmontejo@usf.edu>
2025-09-21 22:04:55 -0400

    Made my items for step 2

---
+++ cc_4a.js
@@ -0,0 +1,8 @@
+// step 2
+const products = [
+    {name:"Soccer Ball", Category:"Sports",price:19.99,inventory:20},
+    {name:"Athletic Shorts", Category:"Clothing",price:9.99,inventory:35},
+    {name:"Laptops", Category:"Technology",price:499.99,inventory:8},
+    {name:"Shampoo", Category:"Essentials",price:4.99,inventory:42},
+    {name:"Chair", Category:"Household",price:49.99,inventory:12},
+];
